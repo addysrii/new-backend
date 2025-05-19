783e816ab2b6149362de91817cd1b0ec51ab6548
addysrii <149177246+addysrii@users.noreply.github.com>
2025-05-19 14:50:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ try {
   };
 }
 
-// app.use('/api/bookings', require('./routes/bookings.routes'));
+app.use('/api/bookings', require('./routes/bookings.routes'));
 app.use('/api/payments', require('./routes/payments.routes'));
 try {
   console.log('Importing cse routes ');
