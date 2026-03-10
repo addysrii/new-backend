e5f9b48665019829578574d46f85dfb66c9573f4
Aditya Srivastava <149177246+addysrii@users.noreply.github.com>
2026-03-10 20:04:58 +0530

    Update network.controller.js

--- controllers/network.controller.js
+++ controllers/network.controller.js
@@ -1,1 +1,48 @@
 
+const ConnectionRequest = require("../models/ConnectionRequest");
+const { User } = require("../models/User");
+
+exports.sendConnectionRequest = async (req, res) => {
+
+ try {
+
+  const sender = req.user.id;
+  const receiver = req.params.userId;
+
+  if (sender === receiver) {
+   return res.status(400).json({
+    error: "Cannot connect with yourself"
+   });
+  }
+
+  const existing = await ConnectionRequest.findOne({
+   sender,
+   receiver,
+   status: "pending"
+  });
+
+  if (existing) {
+   return res.status(400).json({
+    error: "Request already sent"
+   });
+  }
+
+  const request = await ConnectionRequest.create({
+   sender,
+   receiver
+  });
+
+  res.json({
+   message: "Connection request sent",
+   request
+  });
+
+ } catch (err) {
+
+  res.status(500).json({
+   error: "Failed to send request"
+  });
+
+ }
+
+};
