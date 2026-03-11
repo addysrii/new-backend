27c827921567048362a3840c4bfa960a3a792d0b
Aditya Srivastava <149177246+addysrii@users.noreply.github.com>
2026-03-11 18:06:45 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -32,6 +32,6 @@ router.get('/auth/google/callback',
   }),
   authController.googleCallback
 );
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("auth/register", authController.register);
+router.post("auth/login", authController.login);
 module.exports = router
